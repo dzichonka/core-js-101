31200cfcfca686f248257dedd7aeaf2fb7182bad
Anna Vasilevich <dzichonka@gmail.com>
2024-08-24 21:02:56 +0200

    feat: pass 59/17

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -519,8 +519,8 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return Array.from(new Set(arr));
 }
 
 /**
@@ -553,8 +553,17 @@ function distinct(/* arr */) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((res, item) => {
+    const key = keySelector(item);
+    const value = valueSelector(item);
+    if (!res.has(key)) {
+      res.set(key, []);
+    }
+    res.get(key).push(value);
+
+    return res;
+  }, new Map());
 }
 
 
@@ -571,8 +580,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return Array.from(arr, childrenSelector).flat();
 }
 
 
@@ -588,10 +597,19 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  function filter(elem, i) {
+    if (i === indexes.length) {
+      return elem;
+    }
+    return filter(elem[indexes[i]], i + 1);
+  }
+  return filter(arr, 0);
 }
 
+// function getElementByIndexes(arr, indexes) {
+//   return indexes.reduce((acc, curr) => acc[curr], arr);
+// }
 
 /**
  * Swaps the head and tail of the specified array:
@@ -611,8 +629,15 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const lengthOfHalf = Math.floor(arr.length / 2);
+  const head = arr.slice(0, lengthOfHalf);
+  const tail = arr.slice(-lengthOfHalf);
+  const res = [...tail, ...head];
+  if (res.length !== arr.length) {
+    res.splice(lengthOfHalf, 0, arr[lengthOfHalf]);
+  }
+  return res;
 }
 
 
